3908753ec90d4f6bf1792b26cab5dd2a04cfdfc9
Radu Crisan <radu.crisan@ekkode.com>
2025-12-12 11:05:03 +0200

    Add Android Chrome 192x192 icon image

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -10,6 +11,23 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '',
   plugins: [
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        theme_color: "#169bcb",
+        icons: [
+          {
+            src: "icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          }
+        ]
+      }
+    }
+    ),
     vue(),
     tailwindcss(),
     vueDevTools(),
