6c249b52aa761e6eaf20096bab7e5a7372e47dd0
Radu Crisan <radu.crisan@ekkode.com>
2026-03-06 09:05:36 +0000

    feat(api): add authentication and university routing functionality

--- api/api.js
+++ api/api.js
@@ -1,22 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import authRouter from "./router/authRouter.js";
+import universityRouter from "./router/universityRouter.js";
+
 const api = express();
 const port = 3000;
 
-const router = express.Router();
-
-api.use(
-  "/university",
-  router.get("/", (req, res) => {
-    res.send("Hello World!");
-  }),
-  router.get("/an2", (req, res) => {
-    res.send(JSON.stringify({ name: "An2" }));
-  }),
-);
-
 api.use(bodyParser.json());
+
+api.use("/auth", authRouter);
+api.use("/university", universityRouter);
 
 api.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
