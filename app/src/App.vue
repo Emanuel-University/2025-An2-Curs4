9acd41c1bd87f456b95b61569e8477882d1a4d53
Radu Crisan <radu.crisan@ekkode.com>
2026-03-06 09:59:38 +0000

    feat(auth): implement authentication logic and create login page feat(api): add university API endpoints refactor(App.vue): update navigation based on authentication state

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,28 @@
 <script setup>
-
+import { useAuth } from "@/stores/auth"
+import { useRouter } from "vue-router"
+const auth = useAuth()
+const router = useRouter()
 </script>
 
 <template>
- <header>
-     <nav class="flex gap-4 mb-4">
-       <RouterLink to="/">Home</RouterLink>
-       <RouterLink to="/flowers">Flowers</RouterLink>
-       <RouterLink to="/computed">Computed</RouterLink>
-       <RouterLink to="/store">Store Test</RouterLink>
-       <RouterLink to="/tasks">Tasks</RouterLink>
-     </nav>
- </header>
-
- <RouterView />
+  <header v-if="auth.isAuthenticated" class="bg-gray-200 p-4">
+    <nav class="mb-4 flex gap-4">
+      <RouterLink to="/"> Home </RouterLink>
+      <RouterLink to="/flowers"> Flowers </RouterLink>
+      <RouterLink to="/computed"> Computed </RouterLink>
+      <RouterLink to="/store"> Store </RouterLink>
+      <RouterLink to="/tasks"> Tasks </RouterLink>
+    </nav>
+  </header>
+  <RouterView v-if="!auth.isAuthenticated || router.currentRoute.value.path === '/login'" />
 </template>
 
 <style>
-  .btn-primary {
-    background-color: yellow;
-  }
+@import "bootstrap-icons";
+@import "tailwindcss";
+
+.btn-primary {
+  background-color: rgb(192, 235, 238);
+}
 </style>
---
+++ app/src/pages/LoginPage.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { useAuth } from "@/stores/auth"
+import { ref } from "vue"
+const auth = useAuth()
+
+const resultAfterCheckedCredentials = ref(null)
+const authentification = () => {
+  console.log("Authentification function called", username.value, password.value)
+  resultAfterCheckedCredentials.value = auth.checkCredentials(username.value, password.value)
+}
+
+const username = ref("")
+const password = ref("")
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <h1>Login Page</h1>
+    <div>
+      <input v-model="username" type="text" placeholder="Username" class="mb-2 border p-2" />
+    </div>
+
+    <div>
+      <input v-model="password" type="password" placeholder="Password" class="mb-2 border p-2" />
+    </div>
+    <button class="rounded bg-blue-500 px-4 py-2 text-white" @click="authentification">
+      Login
+    </button>
+    <div v-if="resultAfterCheckedCredentials !== null" class="mt-4">
+      <p v-if="resultAfterCheckedCredentials">Login successful!</p>
+      <p v-else>Invalid username or password. Please try again.</p>
+    </div>
+  </div>
+</template>
