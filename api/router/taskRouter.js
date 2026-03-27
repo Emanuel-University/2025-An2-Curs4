e5602e034d28b380a0c4096cc069eab6ad55c632
Radu Crisan <radu.crisan@ekkode.com>
2026-03-27 09:55:19 +0000

    feat(api): implement task management with CRUD operations and database integration

--- api/router/taskRouter.js
+++ api/router/taskRouter.js
@@ -1,34 +1,29 @@
 import { Router } from "express";
+import { Task } from "../database/entities/task.model.js";
 
 const router = Router();
 
-const tasks = [
-  {
-    id: 1,
-    title: "Learn Vue",
-    done: false,
-    favorite: false,
-  },
-  {
-    id: 2,
-    title: "Learn Pinia",
-    done: false,
-    favorite: false,
-  },
-];
+const tasks = [];
 
 router.get("/get-all", (req, res) => {
   res.send(tasks);
 });
 
-router.post("/add", (req, res) => {
-  tasks.push(req.body.task);
+router.post("/add", async (req, res) => {
+  const title = req.body.task;
+
+  await Task.create({
+    title,
+  });
+  tasks.push(title);
   console.log(tasks);
   res.send({ success: true });
 });
 
-router.delete("/delete", (req, res) => {
+router.delete("/delete", async (req, res) => {
   const id = req.body.id;
+
+  await Task.destroy({ where: { id } });
   tasks.splice(
     tasks.findIndex((task) => task.id === id),
     1,
@@ -36,9 +31,22 @@ router.delete("/delete", (req, res) => {
   res.send({ success: true });
 });
 
-router.put("/update-title", (req, res) => {
+router.put("/update-title", async (req, res) => {
   const id = req.body.id;
   const newTitle = req.body.newTitle;
+
+  console.log("id", id);
+  console.log("newTitle", newTitle);
+
+  await Task.update(
+    {
+      title: newTitle,
+    },
+    {
+      where: { id },
+    },
+  );
+
   const index = tasks.findIndex((task) => task.id === id);
   tasks[index].title = newTitle;
   res.send({ success: true });
